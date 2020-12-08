959ff878cf10bab102afd51e8300dbc5cdb27f54
HenryTan0705 <htan0705@gmail.com>
2020-12-08 16:31:49 -0500

    Use set time out to transition between card, loading, and invalid

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -8,43 +8,55 @@ import axios from 'axios';
 })
 export class InfoComponent implements OnInit {
   @Input() searchInput: string;
-  userFound: boolean = true;
+  userFound: boolean = false;
+  displayNotFound: boolean = false;
   data = {};
+  loading: boolean = false;
 
   constructor() {}
 
   showResults() {
-    axios
-      .get(`https://api.github.com/users/${this.searchInput}`)
-      .then(res => {
-        this.userFound = true;
-        console.log(res.data);
-        let {
-          name,
-          login,
-          avatar_url,
-          followers,
-          html_url,
-          public_repos,
-          bio,
-          blog
-        } = res.data;
+    setTimeout(() => {
+      this.loading = true;
+      this.userFound = false;
+      this.displayNotFound = false;
+    }, 0);
 
-        this.data = {
-          name,
-          login,
-          avatar_url,
-          followers,
-          html_url,
-          public_repos,
-          bio,
-          blog
-        };
-      })
-      .catch(err => {
-        this.userFound = false;
-        console.log(err);
-      });
+    setTimeout(() => {
+      axios
+        .get(`https://api.github.com/users/${this.searchInput}`)
+        .then(res => {
+          let {
+            name,
+            login,
+            avatar_url,
+            followers,
+            html_url,
+            public_repos,
+            bio,
+            blog
+          } = res.data;
+
+          this.data = {
+            name,
+            login,
+            avatar_url,
+            followers,
+            html_url,
+            public_repos,
+            bio,
+            blog
+          };
+          this.loading = false;
+          this.userFound = true;
+        })
+        .catch(err => {
+          this.loading = false;
+          this.displayNotFound = true;
+          this.userFound = false;
+          console.log(err);
+        });
+    }, 1000);
   }
 
   ngOnChanges(changes) {
